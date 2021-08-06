50e63930f889aa60d8708bf8190e3cf8a1472473
Bas van der Hoek <bas@hoek.com>
2021-08-07 01:23:35 +0200

    macos only

--- Package.swift
+++ Package.swift
@@ -1,24 +1,20 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
   name: "Resolute",
+  platforms: [
+    .macOS(.v10_15)
+  ],
   products: [
     .library(name: "Resolute", targets: ["Resolute"]),
     .executable(name: "resolute", targets: ["ResoluteCli"])
   ],
   dependencies: [],
   targets: [
-    .target(
-      name: "Resolute",
-      dependencies: []),
-    .target(
-      name: "ResoluteCli",
-      dependencies: ["Resolute"]),
-    .testTarget(
-      name: "ResoluteTests",
-      dependencies: ["Resolute"]),
+    .target(name: "Resolute", dependencies: []),
+    .target(name: "ResoluteCli", dependencies: ["Resolute"]),
+    .testTarget(name: "ResoluteTests", dependencies: ["Resolute"]),
   ]
 )
