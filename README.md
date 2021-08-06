093b7537685eba732b44d85f4ae10564c25fd579
Bas van der Hoek <bas@hoek.com>
2021-08-06 14:46:26 +0200

    spm init library

---
+++ README.md
@@ -0,0 +1,3 @@
+# resolution-swift
+
+A description of this package.
---
+++ Tests/resolution-swiftTests/resolution_swiftTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import resolution_swift
+
+    final class resolution_swiftTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(resolution_swift().text, "Hello, World!")
+        }
+    }
