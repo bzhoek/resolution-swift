5058de76dace1067624f221ffc801391f7f5e404
Bas van der Hoek <bas@hoek.com>
2021-08-07 00:59:45 +0200

    integration test for mbp16 resolution

--- Tests/ResoluteTests/ResoluteTests.swift
+++
@@ -1,8 +0,0 @@
-import XCTest
-@testable import resolution_swift
-
-final class resolution_swiftTests: XCTestCase {
-  func testExample() {
-    XCTAssertEqual(resolution_swift().text, "Hello, World!")
-  }
-}
---
+++ Tests/ResoluteTests/integration.swift
@@ -0,0 +1,20 @@
+import XCTest
+@testable import ResoluteLib
+
+class ScreenResolutionTest: XCTestCase {
+
+  override func setUp() {
+    super.setUp()
+  }
+
+  func testListResolutions() {
+    XCTAssertEqual(Screens().listDisplays(), ["  0  1536 x  960 @ 2x @ 59Hz"])
+  }
+
+  func testUnscaled() {
+    let screens = Screens()
+    screens.saveCurrentMode()
+    XCTAssertEqual([0], screens.savedModes)
+  }
+
+}
